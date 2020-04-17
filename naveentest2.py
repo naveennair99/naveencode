d11620c4e0e9674e7c66a82d68a4160d41ff1212
Naveen <nair.09@gmail.com>
2020-04-17 15:38:01 +0000

    Adding file to remote branch

---
+++ naveentest2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+Money = 2000
+def AddMoney(Money):
+   # Uncomment the following line to fix the code:
+   # global Money
+   Money = 10 + 1
+   print "Money is:", Money
+   return
+
+AddMoney(Money)
