961489b0ae95a9c4c7ef72e24e12fcaea9de20b4
Naveen <nair.09@gmail.com>
2020-06-07 07:14:19 +0000

    New commit

--- naveentest2.py
+++ naveentest2.py
@@ -4,7 +4,7 @@ Money = 2000
 def AddMoney(Money):
    # Uncomment the following line to fix the code:
    # global Money
-   Money = 10 + 1
+   Money = 10 + 2
    print "Money is:", Money
    return
 
